472293506b5e3f032becbc7f0c157a641e32d2e9
ehuntsman <ehuntsman@wavetronix.com>
2016-04-08 21:47:11 -0600

    production environments cache classes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.serve_static_assets = true
-  # config.cache_classes = true
+  config.cache_classes = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
