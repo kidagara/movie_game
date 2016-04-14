c7225e53f4d1d6e689169f8c81380a1bd3188553
ehuntsman <ehuntsman@wavetronix.com>
2016-04-14 10:44:59 -0600

    another precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.serve_static_assets = true
   config.cache_classes = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
